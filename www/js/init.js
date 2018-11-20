8a7227f63cc09d9c189dd48f77dc25a550c09ca7
bsaul <bradleysaul@gmail.com>
2018-11-20 13:22:08 -0500

    link up to remote db

--- www/js/init.js
+++ www/js/init.js
@@ -1,5 +1,7 @@
 /* initialization for testing purposes */
-const db = new PouchDB('http://localhost:5984/test_canopy');
+const db = new PouchDB(
+  'https://ulsentlychomestailsompas:452c3ee1c0917721c6adcc60598f237d0b54e029@4f00102d-b5e6-4d9a-a453-f022fd46d21a-bluemix.cloudant.com/oc_canopy_test'  
+);
 
 db.init = function(){
   return db.info().then(function (details) {

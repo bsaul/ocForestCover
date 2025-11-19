ce24138e2f28e33394643753782106934198cba3
Bradley Saul <bradleysaul@fastmail.com>
2025-11-19 16:02:39 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "OC Forest Cover";
+  nixConfig = {
+    bash-prompt = "λ ";
+  };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {      
+      
+      formatter = pkgs.nixfmt-rfc-style;
+
+      checks.whitespace = pkgs.stdenvNoCC.mkDerivation {
+        name = "check-whitespace";
+        dontBuild = true;
+        src = ./.;
+        doCheck = true;
+        checkPhase = ''
+          ${pkgs.haskellPackages.fix-whitespace}/bin/fix-whitespace --check
+        '';
+        installPhase = ''mkdir "$out"'';
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          # Stats
+          pkgs.R
+          # Development tools
+          pkgs.flyctl
+          pkgs.nodePackages.prettier
+          pkgs.nodePackages.eslint
+          pkgs.updog
+
+          # Code tools
+          pkgs.haskellPackages.fix-whitespace
+        ];
+      }; 
+    });
+}
